e15e110539f763a5cdc21c20a18e234b221cb05f
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-08-01 13:22:29 +0200

    fix: Only load server-side modules on server

--- init.lua
+++ init.lua
@@ -1,12 +1,14 @@
 local m = {}
 
 m.Chat = SELENE_IS_SERVER and require("moonlight.server.lua.chat") or require("moonlight.client.lua.chat")
-m.Persistence = require("moonlight.server.lua.persistence")
-m.Permissions = require("moonlight.server.lua.permissions")
-m.Attributes = require("moonlight.server.lua.attributes")
-m.Skills = require("moonlight.server.lua.skills")
-m.Actions = require("moonlight.server.lua.actions")
-m.Combat = require("moonlight.server.lua.combat")
-m.Dialogs = require("moonlight.server.lua.dialogs")
+if SELENE_IS_SERVER then
+    m.Persistence = require("moonlight.server.lua.persistence")
+    m.Permissions = require("moonlight.server.lua.permissions")
+    m.Attributes = require("moonlight.server.lua.attributes")
+    m.Skills = require("moonlight.server.lua.skills")
+    m.Actions = require("moonlight.server.lua.actions")
+    m.Combat = require("moonlight.server.lua.combat")
+    m.Dialogs = require("moonlight.server.lua.dialogs")
+end
 
 return m
